6588251fddd3f6c0a81a4cfe78c26ff0a7573e7c
Adams Anthony Isioma <114524110+AthonysWorld@users.noreply.github.com>
2023-05-25 19:48:14 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-var observer= new IntersectionObserver((entries)=>{
-entries.forEach((entry)=>{
-    console.log(entry)
-    if(entry.isIntersecting){
-        entry.target.classList.add(`show`);
-    }else{
-        entry.target.classList.remove(`show`);
-    }
-})
-});
+// var observer= new IntersectionObserver((entries)=>{
+// entries.forEach((entry)=>{
+//     console.log(entry)
+//     if(entry.isIntersecting){
+//         entry.target.classList.add(`show`);
+//     }else{
+//         entry.target.classList.remove(`show`);
+//     }
+// })
+// });
 
-var hiddenElements=document.querySelectorAll( hidden);
-hiddenElements.forEach((el)=> observer(el));
+// var hiddenElements=document.querySelectorAll( hidden);
+// hiddenElements.forEach((el)=> observer(el));
+
+// const openButton = document.getElementByCl('openButton');
+
+// openButton.addEventListener('click', () => {
+//   // Replace 'path_to_your_document' with the actual path to your document
+//   const documentPath = 'path_to_your_document';
+//   window.open(documentPath, '_blank');
+// });
